a21b6b7261cab165642e540d424eae92471f796d
Lincyaw <814750204@qq.com>
2020-10-24 20:09:30 +0800

    Finish dealing functions

--- js/notes.js
+++ js/notes.js
@@ -16,7 +16,7 @@
             "item": ""
         }
     ]
-    member, merchant,item   are not defined, those function for them will return as following:
+    member, merchant,item are not defined, those function for them will return as following:
 
     undefined: [
     {
@@ -26,9 +26,24 @@
       flag: '',
       ...
     }]
-
+    ============================================================================================
+    |你主要将会使用  viewByTimeLine(); 以及  billFilter();
+    |viewByTimeLine()只需要输入bills即可，billFilter()需要你仔细看一看。输入的bills需要保证按时间有序。
+    |使用billFilter()函数输出一个过滤后的bills，再传入viewByTimeLine()函数，可以使用户筛选的对应的数据
+    |按三级目录的形式输出。
+    |
+    |亦或者使用billFilter()函数，配合viewByMainCat，viewBySubCat等一系列的查看函数，输出按对应函数的
+    |顺序（注意：这些函数是平面的，并没有像时间那样可以有三级的目录表示）
+    |===========================================================================================
  */
-
+class BillType {
+    static get income() {
+        return 1;
+    }
+    static get expense() {
+        return 2;
+    }
+}
 class singleList {
     constructor(token) {
         this.token = token;
@@ -38,14 +53,46 @@ class singleList {
         this.list = [];
     }
 }
-
+/**
+ * Returns a sorted list as following.
+ *{
+    "token": "year-month-day",
+    "income": 200,
+    "expense": 400,
+    "remain": -200,
+    "list": [{
+        "token": "2020",
+        "income": 200,
+        "expense": 400,
+        "remain": -200,
+        "list": [{
+            "title": "9",
+            "income": 200,
+            "expense": 400,
+            "remain": -200,
+            "list": [{
+                "title": "28",
+                "income": 200,
+                "expense": 400,
+                "remain": -200,
+                "list": [{
+                    // this is a bill
+                }]
+            }]
+        }]
+    }]
+}
+ * @param {list} bills
+ */
 function viewByTimeLine(bills) {
     let viewByTime = new singleList("year-month-day");
     for (var bill of bills) {
-        var year = viewByTime.list.findIndex(function (t) {  // 找有没有xx年的记录
+        var year = viewByTime.list.findIndex(function (t) {
+            // 找有没有xx年的记录
             return t.token == getDate(bill.time, choices.year);
         });
-        if (year == -1) {  // 没有xx年的记录
+        if (year == -1) {
+            // 没有xx年的记录
             var viewByYear = new singleList(getDate(bill.time, choices.year));
             var viewByMonth = new singleList(getDate(bill.time, choices.month));
             var viewByDay = new singleList(getDate(bill.time, choices.day));
@@ -71,11 +118,15 @@ function viewByTimeLine(bills) {
             viewByYear.list.push(viewByMonth);
             viewByTime.list.push(viewByYear);
         } else {
-            var month = viewByTime.list[year].list.findIndex(function (t) {  // 找有没有xx月的记录
+            var month = viewByTime.list[year].list.findIndex(function (t) {
+                // 找有没有xx月的记录
                 return t.token == getDate(bill.time, choices.month);
             });
-            if (month == -1) {// 没有xx月的记录
-                var viewByMonth = new singleList(getDate(bill.time, choices.month));
+            if (month == -1) {
+                // 没有xx月的记录
+                var viewByMonth = new singleList(
+                    getDate(bill.time, choices.month)
+                );
                 var viewByDay = new singleList(getDate(bill.time, choices.day));
 
                 if (bill.type == BillType.income) {
@@ -91,18 +142,26 @@ function viewByTimeLine(bills) {
                 }
                 viewByDay.remain = viewByDay.income - viewByDay.expense;
                 viewByMonth.remain = viewByMonth.income - viewByMonth.expense;
-                viewByTime.list[year].remain = viewByTime.list[year].income - viewByTime.list[year].expense;
+                viewByTime.list[year].remain =
+                    viewByTime.list[year].income -
+                    viewByTime.list[year].expense;
                 viewByTime.remain = viewByTime.income - viewByTime.expense;
 
                 viewByDay.list.push(bill);
                 viewByMonth.list.push(viewByDay);
                 viewByTime.list[year].list.push(viewByMonth);
             } else {
-                var day = viewByTime.list[year].list[month].list.findIndex(function (t) {  // 找有没有xx日的记录
-                    return t.token == getDate(bill.time, choices.day);
-                });
-                if (day == -1) {// 没有xx日的记录
-                    var viewByDay = new singleList(getDate(bill.time, choices.day));
+                var day = viewByTime.list[year].list[month].list.findIndex(
+                    function (t) {
+                        // 找有没有xx日的记录
+                        return t.token == getDate(bill.time, choices.day);
+                    }
+                );
+                if (day == -1) {
+                    // 没有xx日的记录
+                    var viewByDay = new singleList(
+                        getDate(bill.time, choices.day)
+                    );
                     if (bill.type == BillType.income) {
                         viewByDay.income += bill.amount;
                         viewByTime.list[year].list[month].income += bill.amount;
@@ -110,32 +169,47 @@ function viewByTimeLine(bills) {
                         viewByTime.income += bill.amount;
                     } else if (bill.type == BillType.expense) {
                         viewByDay.expense += bill.amount;
-                        viewByTime.list[year].list[month].expense += bill.amount;
+                        viewByTime.list[year].list[month].expense +=
+                            bill.amount;
                         viewByTime.list[year].expense += bill.amount;
                         viewByTime.expense += bill.amount;
                     }
                     viewByDay.remain = viewByDay.income - viewByDay.expense;
-                    viewByTime.list[year].list[month].remain = viewByTime.list[year].list[month].income - viewByTime.list[year].list[month].expense;
-                    viewByTime.list[year].remain = viewByTime.list[year].income - viewByTime.list[year].expense;
+                    viewByTime.list[year].list[month].remain =
+                        viewByTime.list[year].list[month].income -
+                        viewByTime.list[year].list[month].expense;
+                    viewByTime.list[year].remain =
+                        viewByTime.list[year].income -
+                        viewByTime.list[year].expense;
                     viewByTime.remain = viewByTime.income - viewByTime.expense;
 
                     viewByDay.list.push(bill);
                     viewByTime.list[year].list[month].list.push(viewByDay);
-                } else { // 全部找到了
+                } else {
+                    // 全部找到了
                     if (bill.type == BillType.income) {
-                        viewByTime.list[year].list[month].list[day].income += bill.amount;
+                        viewByTime.list[year].list[month].list[day].income +=
+                            bill.amount;
                         viewByTime.list[year].list[month].income += bill.amount;
                         viewByTime.list[year].income += bill.amount;
                         viewByTime.income += bill.amount;
                     } else if (bill.type == BillType.expense) {
-                        viewByTime.list[year].list[month].list[day].expense += bill.amount;
-                        viewByTime.list[year].list[month].expense += bill.amount;
+                        viewByTime.list[year].list[month].list[day].expense +=
+                            bill.amount;
+                        viewByTime.list[year].list[month].expense +=
+                            bill.amount;
                         viewByTime.list[year].expense += bill.amount;
                         viewByTime.expense += bill.amount;
                     }
-                    viewByTime.list[year].list[month].list[day].remain = viewByTime.list[year].list[month].list[day].income - viewByTime.list[year].list[month].list[day].expense;
-                    viewByTime.list[year].list[month].remain = viewByTime.list[year].list[month].income - viewByTime.list[year].list[month].expense;
-                    viewByTime.list[year].remain = viewByTime.list[year].income - viewByTime.list[year].expense;
+                    viewByTime.list[year].list[month].list[day].remain =
+                        viewByTime.list[year].list[month].list[day].income -
+                        viewByTime.list[year].list[month].list[day].expense;
+                    viewByTime.list[year].list[month].remain =
+                        viewByTime.list[year].list[month].income -
+                        viewByTime.list[year].list[month].expense;
+                    viewByTime.list[year].remain =
+                        viewByTime.list[year].income -
+                        viewByTime.list[year].expense;
                     viewByTime.remain = viewByTime.income - viewByTime.expense;
 
                     viewByTime.list[year].list[month].list[day].list.push(bill);
@@ -146,37 +220,35 @@ function viewByTimeLine(bills) {
     return viewByTime;
 }
 
-
-
 var choices = {
     day: 0,
     month: 1,
     year: 2,
-    quarter: 3
+    quarter: 3,
 };
 /**
  * You will not use this function.
- * @param {*} date 
- * @param {*} choice 
+ * @param {*} date
+ * @param {*} choice
  */
 function getDate(date, choice) {
     var curYear = date.getFullYear();
     var curMonth = date.getMonth() + 1;
     var curDate = date.getDate();
     if (curMonth < 10) {
-        curMonth = '0' + curMonth;
+        curMonth = "0" + curMonth;
     }
     if (curDate < 10) {
         curDate = "0" + curDate;
     }
     if (choice == choices.day) {
-        return (curYear + "-" + curMonth + "-" + curDate);
+        return curYear + "-" + curMonth + "-" + curDate;
     } else if (choice == choices.month) {
-        return (curYear + "-" + curMonth);
+        return curYear + "-" + curMonth;
     } else if (choice == choices.year) {
-        return (curYear);
+        return curYear;
     } else if (choice == choices.quarter) {
-        return (curYear + "-" + Math.floor(curMonth / 3));
+        return curYear + "-" + Math.floor(curMonth / 3);
     } else {
         // alert('Input not allowed.');
     }
@@ -184,9 +256,9 @@ function getDate(date, choice) {
 /**
  * use this function as following:
  * var ans = viewByHour(bills);
- * 
+ *
  * ans[8] contains a array of all the bills in 8'clock.
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByHour(bills) {
     var ans = new Array(24);
@@ -194,13 +266,13 @@ function viewByHour(bills) {
         ans[i] = new Array();
     }
     for (var i = 0; i < bills.length; i++) {
-        ans[bills[i].time.getHours()].push(bills[i])
+        ans[bills[i].time.getHours()].push(bills[i]);
     }
     return ans;
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByDay(bills) {
     var ans = new Object();
@@ -214,16 +286,8 @@ function viewByDay(bills) {
 }
 
 /**
- * todo;
- * Viewing by week is tricky. 
- * @param {array} bills 
- */
-function viewByWeek(bills) {
-
-}
-/**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByMonth(bills) {
     var ans = new Object();
@@ -237,7 +301,7 @@ function viewByMonth(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByQuarter(bills) {
     var ans = new Object();
@@ -251,7 +315,7 @@ function viewByQuarter(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByYear(bills) {
     var ans = new Object();
@@ -265,7 +329,7 @@ function viewByYear(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByMainCat(bills) {
     var ans = new Object();
@@ -279,7 +343,7 @@ function viewByMainCat(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewBySubCat(bills) {
     var ans = new Object();
@@ -293,7 +357,7 @@ function viewBySubCat(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByAccount(bills) {
     var ans = new Object();
@@ -307,7 +371,7 @@ function viewByAccount(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByItem(bills) {
     var ans = new Object();
@@ -321,7 +385,7 @@ function viewByItem(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByMember(bills) {
     var ans = new Object();
@@ -335,7 +399,7 @@ function viewByMember(bills) {
 }
 /**
  *  Return a Dictionary/Object including arrays
- * @param {array} bills 
+ * @param {array} bills
  */
 function viewByMerchant(bills) {
     var ans = new Object();
@@ -348,20 +412,19 @@ function viewByMerchant(bills) {
     return ans;
 }
 
-
 /**
  * Please make sure input is correct. Call this function as following:
- * 
+ *
  * filter(bills=bills, Object = ob)
- * 
+ *
  * Object has these attributes.
- * @param {array} bills 
+ * @param {array} bills
  * @param {string} account : [acc1,acc2,acc3, ...]   $
  * @param {numberRange} amount : [start, end]        *
  * @param {int} type , true: income; flase: cost    %
  * @param {string} flag , defined: name of account   %
  * @param {string} mainCat , [cat1, cat2,...,catn]   $
- * @param {string} subCat , [cat1, cat2,...,catn]    $ 
+ * @param {string} subCat , [cat1, cat2,...,catn]    $
  * @param {dateRange} time : [startDate, endDate]    *
  * @param {string} member : [mem1, mem2, mem3,...]   $
  * @param {string} merchant : [mer1, mer2,...]       $
@@ -373,43 +436,55 @@ function billFilter(bills, object) {
         if (object[key] == null) {
             continue;
         } else {
-            tempOb[key] = object[key]; // Get all the defined attributes 
+            tempOb[key] = object[key]; // Get all the defined attributes
         }
     }
     var ans = [];
     for (var bill of bills) {
-        var f = false;
+        var f = {};
         for (var key of Object.keys(tempOb)) {
             // 如果是flag(单个字符串) 或者 type
-
-            if ((key == 'flag' || key == 'type')) {
+            f[String(key)] = false;
+            if (key == "flag" || key == "type") {
                 if (bill[key] === tempOb[key]) {
-                    f = true;
+                    f[key] = true;
                 } else {
-                    f = false;
-                    break;
+                    f[key] = false;
                 }
-            } else if (key == 'time' || key == 'amount') { // 如果是日期或者金额范围
-                if (bill[key] >= tempOb[key][0] && bill[key] <= tempOb[key][1]) {
-                    f = true;
+            } else if (key == "time" || key == "amount") {
+                // 如果是日期或者金额范围
+                if (
+                    bill[key] >= tempOb[key][0] &&
+                    bill[key] <= tempOb[key][1]
+                ) {
+                    f[key] = true;
                 } else {
-                    f = false;
-                    break;
+                    f[key] = false;
                 }
-            } else { // 其余的数组类型
-                f = function (tempOb, bill, key) {
-                    for (var i of tempOb[key]) {
-                        if (bill[key] == i) {
-                            return true;
-                        }
+            } else {
+                // 其余的数组类型
+                for (var i of tempOb[key]) {
+                    f[key] = false;
+                    if (bill[key] == i) {
+                        f[key] = true;
                     }
-                    return false;
                 }
             }
         }
-        if (f) {
+        // console.log(typeof key, key, bill[key]);
+        // console.log("f = ", f);
+        var flag = true;
+        for (var key of Object.keys(tempOb)) {
+            flag &= f[key];
+        }
+        if (flag) {
             ans.push(bill);
         }
     }
     return ans;
 }
+
+// module.exports = {
+//     billFilter,
+//     viewByTimeLine,
+// };
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,10 @@
-'use strict';
+"use strict";
 
 class BillType {
-    static get income() { return 1; }
-    static get expense() { return 2; }
+    static get income() {
+        return 1;
+    }
+    static get expense() {
+        return 2;
+    }
 }
