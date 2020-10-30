ad9f756d6c8148e546eb214b4baa47acf6ee8c53
Wang_R <1371164370@qq.com>
2020-10-30 23:40:55 +0800

    添加页面的跳转，主界面底部空白

--- js/bill_waters.js
+++ js/bill_waters.js
@@ -23,33 +23,27 @@ class year {
         this.tagID = tagID;
     }
     create() {
-        // alert("year");
 
-        var year_card = $("<div class='card mt-3 ml-1 mr-1'></div>");
-        
+        var year_card = $("<div class='card mt-3 ml-1'></div>");
+
         var card_body = $("<div class='card-body'></div>");
-        var body_button = $("<button class='btn btn-block btn-link zebra-list-button pl-2 pr-0' type='button' data-toggle='collapse'" + " data-target='#" + this.tagID + "'></button>");
-        var button_contain = $("<div class='container p-0'></div>");
-        var contain_row = $("<div class='row'></div>");
+        var body_button = $("<a class='btn btn-block btn-link zebra-list-button pl-2 pr-0 ' type='button' data-toggle='collapse'" + " data-target='#" + this.tagID + "'></a>");
+        var button_row = $("<div class='row d-flex'></div>");
 
-        var row_col3 = $("<div class='col-3 text-center m-0' style='font-size:x-large;'></div>").html(this.year);
-        var row_col9 = $("<div class='col-9 text-left' style='font-size:small;'>").html("结余:" + this.remain + "<br>" + "收入:" + this.income + "|" + "支出:" + this.expense)
+        var row_col3 = $("<div class='col-3 text-center m-0 p-0' style='font-size:x-large;'></div>").html(this.year);
+        var row_col9 = $("<div class='col-auto text-left p-0' style='font-size:small;'></div>").html("结余:" + this.remain + "<br>" + "收入:" + this.income + "|" + "支出:" + this.expense)
 
         var card_collapse = $("<div class='collapse' style='background-color: whitesmoke;' id='" + this.tagID + "'></div>");
 
-        contain_row.append(row_col3, row_col9);
-        button_contain.append(contain_row);
-        body_button.append(button_contain);
+        button_row.append(row_col3, row_col9);
+        body_button.append(button_row);
         card_body.append(body_button);
 
         // collapse content
-        var tempmonth = new billList(this.dataList,this.tagID);
+        var tempmonth = new billList(this.dataList, this.tagID);
         card_collapse.append(tempmonth.create(month));
 
         year_card.append(card_body, card_collapse);
-        
-        // console.log("year_card");
-        // console.log(year_card);
 
         return year_card;
     }
@@ -65,15 +59,15 @@ class month {
     }
     create() {
         // alert("month");
-        var month_card = $("<div class='card mt-3 ml-1 mr-1'></div>");
+        var month_card = $("<div class='card mt-3 ml-1'></div>");
 
         var card_body = $("<div class='card-body'></div>");
-        var body_button = $("<button class='btn btn-block btn-link zebra-list-button pl-2 pr-0' type='button' data-toggle='collapse'" + " data-target='#"+this.tagID + "'></button>");
+        var body_button = $("<button class='btn btn-block btn-link zebra-list-button pl-2 pr-0' type='button' data-toggle='collapse'" + " data-target='#" + this.tagID + "'></button>");
         var button_contain = $("<div class='container p-0'></div>");
         var contain_row = $("<div class='row contain_row'></div>");
 
-        var row_col3 = $("<div class='col-3 text-center m-0 dropdown-date' style='font-size: large;'></div>").html(parseInt(this.month.slice(5), 10)+"月");
-        var row_col9 = $("<div class='col-9 text-left dropdown-total' style='font-size: small;'>").html("结余:" + this.remain + "<br>" + "收入:" + this.income + "|" + "支出:" + this.expense)
+        var row_col3 = $("<div class='col-3 text-center m-0 dropdown-date' style='font-size: large;'></div>").html(parseInt(this.month.slice(5), 10) + "月");
+        var row_col9 = $("<div class='col-9 text-left dropdown-total p-0' style='font-size: small;'></div>").html("结余:" + this.remain + "<br>" + "收入:" + this.income + "|" + "支出:" + this.expense)
 
         var card_collapse = $("<div class='collapse' style='background-color: whitesmoke;' id='" + this.tagID + "'></div>");
 
@@ -90,8 +84,8 @@ class month {
                 }
             );
         });
-        
-        var last_layer_bills = new billList(bills,this.tagID);
+
+        var last_layer_bills = new billList(bills, this.tagID);
         card_collapse.append(last_layer_bills.create(card));
 
         month_card.append(card_body, card_collapse);
@@ -99,18 +93,45 @@ class month {
     }
 }
 
-function updateAllListByTimeLine(jqueryContainer,data){
+function updateAllListByTimeLine(jqueryContainer, bills, ob) {
     // alert("update", typeof(data));
-
-    $('#account-type').html((data.account)?data.account:"账户:所有账户");
-    $('#account-income').html(data.income+"<br>"+"收入");
-    $('#account-remain').html(data.remain+"<br>"+"结余");
-    $('#account-expense').html(data.expense+"<br>"+"支出");
+    var after_filter = viewByTimeLine(billFilter(bills, ob));
+    var account = ob.account;
+    if (ob.account == null || ob.account[0] == null) {
+        $('#account-type').html("账户:" + "所有账户");
+    } else {
+        $('#account-type').html("账户:" + ob.account[0]);
+    }
+    $('#account-income').html(after_filter.income + "<br>" + "收入");
+    $('#account-remain').html(after_filter.remain + "<br>" + "结余");
+    $('#account-expense').html(after_filter.expense + "<br>" + "支出");
 
 
     // console.log("data");
     // console.log(data);
-    
-    var listContainer = new billList(data.list,"Bill");
+
+    var listContainer = new billList(after_filter.list, "Bill");
     jqueryContainer.append(listContainer.create(year));
+}
+/**
+ * 
+ * url 实例：
+ *
+ *  http://www.runoob.com/index.php?id=1&image=awesome.jpg
+ *  调用 getQueryVariable("id") 返回 1。
+ *
+ *  调用 getQueryVariable("image") 返回 "awesome.jpg"。
+ */
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            
+            var gbk = decodeURI(pair[1]);
+            return gbk;
+        }
+    }
+    return (false);
 }
